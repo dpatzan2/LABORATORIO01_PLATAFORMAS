05233b24d36d56b315d3a7ef591d0ca2b0e26110
Diego Patzan <diegopatzan24@gmail.com>
2024-08-02 07:01:00 -0600

    Adding texts and button function

--- src/main/java/com/uvg/example/ejemplo01_kotlin/MainActivity.kt
+++ src/main/java/com/uvg/example/ejemplo01_kotlin/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.uvg.example.ejemplo01_kotlin.ui.theme.Ejemplo01_kotlinTheme
 
@@ -32,8 +38,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun MyAlertDialog(shouldShowDialog: MutableState<Boolean>) {
+    if (shouldShowDialog.value) { // 2
+        AlertDialog( // 3
+            onDismissRequest = { // 4
+                shouldShowDialog.value = false
+            },
+            // 5
+            title = { Text(text = "Alert Dialog") },
+            text = { Text(text = "Jetpack Compose Alert Dialog") },
+            confirmButton = { // 6
+                Button(
+                    onClick = {
+                        shouldShowDialog.value = false
+                    }
+                ) {
+                    Text(
+                        text = "Confirm",
+                        color = Color.White
+                    )
+                }
+            }
+        )
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val shouldShowDialog = remember { mutableStateOf(false) } // 1
+
     Column (
         modifier = modifier
     ) {
@@ -41,11 +77,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "Hello $name!",
             modifier = modifier
         )
-    
+        Text(
+            text = "No se que poner XD",
+            modifier = modifier
+        )
+        Text(
+            text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            modifier = modifier
+        )
+
+        if (shouldShowDialog.value) {
+            MyAlertDialog(shouldShowDialog = shouldShowDialog)
+        }
+
+        Button(
+            onClick = { shouldShowDialog.value = true },
+            modifier = Modifier
+        ) {
+            Text(text = "Show Dialog")
+        }
     }
 }
 
-@Preview(showBackground = true, showSystemui = true)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ejemplo01_kotlinTheme {
